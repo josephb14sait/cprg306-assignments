cfc930b6299c2287b3a82eee2a2cab76b0314507
josephb14sait <152675414+josephb14sait@users.noreply.github.com>
2024-09-24 16:04:57 -0600

    Finished Week 2 Assignment
    Created week 2 page, student info component, rendered the student info into page 2, and changed the root page.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li><Link href="./week-2/">Week 2 Assignment</Link></li>
+      </ul>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+//react component - name and github link
+
+import Link from "next/link";
+
+export default function StudentInfo() {
+
+    return (
+        <div>
+            <h3>Joseph Bui</h3>
+            <Link href="https://github.com/josephb14sait/cprg306-assignments">https://github.com</Link>
+        </div>
+    );
+}
